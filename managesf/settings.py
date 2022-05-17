6e85f3af64b49652bc418bb265c0e13251f01d39
AbhijitBarui <abhijitongit@gmail.com>
2022-05-17 07:36:38 +0530

    dbfix

--- managesf/settings.py
+++ managesf/settings.py
@@ -79,16 +79,24 @@ WSGI_APPLICATION = 'managesf.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'managesfdb',
-        'USER':'abhijit',
-        'PASSWORD':config('DB_PASSWRD'),
-        'HOST':'localhost',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'managesfdb',
+#         'USER':'abhijit',
+#         'PASSWORD':config('DB_PASSWRD'),
+#         'HOST':'localhost',
+#     }
+# }
 
 
 # Password validation
